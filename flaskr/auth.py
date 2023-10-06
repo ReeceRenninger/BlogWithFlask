068331c3a3e7328d76f5bf9e3d98db034b3cc527
ReeceRenninger <renningerreece@gmail.com>
2023-10-06 14:50:43 -0700

    built out login required views along with logout function, templates are next

--- flaskr/auth.py
+++ flaskr/auth.py
@@ -10,7 +10,7 @@ from flaskr.db import get_db
 
 bp = Blueprint('auth', __name__, url_prefix='/auth') # creates a Blueprint named 'auth'. Like the application object, the blueprint needs to know where it's defined, so __name__ is passed as the second argument. The url_prefix will be prepended to all the URLs associated with the blueprint
 
-#TODO: write a view function for registering a new user
+#** Register endpoint for users to create a new account
 @bp.route('/register', methods=('GET', 'POST'))
 def register():
     if request.method == 'POST':
@@ -40,7 +40,7 @@ def register():
 
     return render_template('auth/register.html') #TODO: NEED TO BUILD THIS TEMPLATE FILE
 
-#TODO: write a login function that will validate the username and password and either return an error or log the user in and redirect them to the index page
+#** Login endpoint for registered users
 @bp.route('/login', methods=('GET', 'POST'))
 def login():
     if request.method == 'POST':
@@ -63,5 +63,33 @@ def login():
             return redirect(url_for('index')) # redirect to the index page
         
         flash(error)
-        
+
     return render_template('auth/login.html') #TODO: NEED TO BUILD THIS TEMPLATE FILE
+
+def login_required(view):
+    @functools.wraps(view) # preserves the original function’s name and documentation
+    def wrapped_view(**kwargs): # a new view function that wraps the original view it’s applied to. The new function checks if a user is loaded and redirects to the login page otherwise. If a user is loaded the original view is called and continues normally. You’ll use this decorator when writing the blog views.
+        if g.user is None:
+            return redirect(url_for('auth.login'))
+        
+        return view(**kwargs) # view function is called with the original arguments
+    
+    return wrapped_view # returns the new view function
+
+@bp.before_app_request # registers a function that runs before the view function, no matter what URL is requested
+def load_logged_in_user():
+    #! we are storing the user id in the session above in the login function to be used here
+    user_id = session.get('user_id') # check if a user id is stored in the session and get that user’s data from the database, storing it on g.user, which lasts for the length of the request. If there is no user id, or if the id doesn’t exist, g.user will be None.
+
+    if user_id is None:
+        g.user = None
+    else:
+        g.user = get_db().execute(
+            'SELECT * FROM user WHERE id = ?', (user_id,)
+        ).fetchone()
+
+#** Logout endpoint for registered users
+@bp.route('/logout')
+def logout():
+    session.clear()
+    return redirect(url_for('index')) # redirect to the index page
